841c8cd27c64c99c656a3b20ce49e59cabbce700
Ren <khaindpd08774@fpt.edu.vn>
2023-02-18 13:45:39 +0700

    khoi tao

---
+++ assignment/assignment1.cpp
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void menu(){
+	int luachon;
+	printf("------MENU------\n");
+	printf("1.  Kiem tra so nguyen.\n");
+	printf("2.  Tim uoc so chung va boi so chung cua 2 so.\n");
+	printf("3.  Chuong trinh tinh tien cho quan karaoke.\n");
+	printf("4.  Tinh tien dien.\n");
+	printf("5.  Chuc nang doi tien.\n");
+	printf("6.  Tinh lai suat vay ngan hang vay tra gop.\n");
+	printf("7.  Chuong trinh vay tien mua xe.\n");
+	printf("8.  Sap xep thong tin sinh vien.\n");
+	printf("9.  Game FPOLY-LOTT(2-15).\n");
+	printf("10. Tinh toan phan so.\n");
+	printf("11. Thoat.\n");
+	printf("Xin moi lua chon chuc nang:");
+	scanf("%d",&luachon);
+	printf("----------------\n");
+	switch(luachon){
+		case 1:
+			printf("Day la so nguyen.\n");
+			printf("Day khong phai la so nguyen to.\n");
+			printf("Day khong phai la so chinh phuong.\n");
+		break;
+		case 2:
+			printf("Uoc so chung lon nhat:\n");
+			printf("Boi so chung nho nhat:\n");
+		break;
+		case 3:
+			printf("So tien can phai thanh toan:\n");
+		break;
+		case 4:
+			printf("So tien can phai tra:\n");
+		break;
+		case 5:
+			printf("So tien da duoc doi thanh:\n");
+		break;
+		case 6:
+			printf("So tien can tra trong 12 thang:\n");
+		break;
+		case 7:
+			printf("So tien phai tra lan dau:\n");
+			printf("So tien phai tra hang thang:\n");
+		break;
+		case 8:
+			printf("Day la thong tin cua ban:\n");
+		break;
+		case 9:
+			printf("Chuc ban may man lan sau.\n");
+		break;
+		case 10:
+			printf("Tong cua 2 phan so:\n");
+			printf("Hieu cua 2 phan so:\n");
+			printf("Tich cua 2 phan so:\n");
+			printf("Thuong cua 2 phan so:\n");
+		break;
+		case 11:
+			exit(0);
+		default:
+			printf("Vui long chon 1 trong cac chuc nang o tren.\n");
+	}
+}
+
+int main(){
+	while(1){
+		menu();
+	}
+}
